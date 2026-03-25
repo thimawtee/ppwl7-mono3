02e37935a5482962e9921cb46266b54b9c58063b
thimawtee <timothyketapang@gmail.com>
2026-03-25 18:32:10 +0700

    monorepo 3 telah selesai

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,10 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
 // import App from './ApiTest'
-import App from './App2'
+// import App from './App2'
+
+// Routing sederhana berdasarkan path
+const path = window.location.pathname
+
+let App
+if (path === '/classroom') {
+  const { default: ClassroomApp } = await import('./App3')
+  App = ClassroomApp
+} else {
+  const { default: DefaultApp } = await import('./App2')
+  App = DefaultApp
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
